243d6ebfa5f99623a3f2f4b8f9ed4cce3aa5b8aa
evanfeng <evanfeng@pku.edu.cn>
2018-08-14 15:40:32 +0800

    add CorpusReader, add <BOS> <EOS> tokens

--- train.py
+++ train.py
@@ -4,21 +4,24 @@ Memory-efficient language modeling via probabilistic counting.
 Programmer: fyl
 Date: 2018/8/13
 """
-import argparse
 import re
 import os
-import frequency_estimation
+import argparse
 import pickle
-from pprint import pprint
+import logging
+import frequency_estimation
 
 PUNCS = ',.=[]{}/\\<>!@#$%^&*()-+_|`~"'
 
-def tokenize(line):
+
+def tokenize(line, ngram_size):
     """
     Helper function for tokenizing a single line of text.
 
     line: str
         a line of text to be tokenized
+    ngram_size: int
+        (ngram_size - 1) <BOS> tokens will be added at the begining of the sentence
 
     Returns: list[str]
         a list of tokens
@@ -27,48 +30,75 @@ def tokenize(line):
     line = line.lower()
     for ch in PUNCS:
         line = line.replace(ch, ' ')
+    line = line.strip()
 
     # replace digits with a <NUM> token
     line = re.sub(r'\b\d+\b', '<NUM>', line)
 
     tokens = line.split()
-    tokens.append('<EOS>')
+    tokens = ['<BOS>'] * (ngram_size - 1) + tokens + ['<EOS>']
     return tokens
 
 
-
-def corpus_reader(corpus_path, ngram_size, encoding='utf-8'):
+class CorpusReader(object):
     """
-    Helper function for scanning over the entire corpus and generating ngrams on the fly. We
+    Helper class for scanning over the entire corpus and generating ngrams on the fly. We
     assume that a single line of the corpus can still be loaded into the memory even if the
     entire corpus is large.
 
+    Parameters
+    ----------
     corpus_path: str
         use the specified corpus to train the model
     ngram_size: int
         ngrams of size ngram_size and (ngram_size - 1) will be generated
     encoding: str, optional (default: utf-8)
         the encoding method of the corpus file
-
-    Returns: generator
-        a python generator that yields ngram tuples on the fly
     """
-    with open(corpus_path, 'r', encoding=encoding) as fin:
-        for line in fin:
-            # tokenize
-            words = tokenize(line)
 
-            # skip lines that are too short to produce proper ngrams
-            if len(words) < ngram_size - 1:
-                continue
+    def __init__(self, corpus_path, ngram_size, encoding='utf-8'):
+        self.corpus_path = corpus_path
+        self.ngram_size = ngram_size
+        self.encoding = encoding
+        self.vocabulary = set()
+        self.vocab_size = len(self.vocabulary)
 
-            # yield ngrams of size ngram_size and (ngram_size - 1)
-            for offset in range(0, len(words) - ngram_size + 1):
-                yield tuple(words[offset:offset + ngram_size])
-                yield tuple(words[offset:offset + ngram_size - 1])
+    def __iter__(self):
+        """
+        Returns: generator
+            a python generator that yields ngram tuples on the fly
+        """
+        return self._read_corpus()
+
+    def _read_corpus(self):
+        """
+        Scanning over the entire corpus and generate ngrams on the fly.
+
+        Returns: generator
+            a python generator that yields ngram tuples on the fly
+        """
+        ngram_size = self.ngram_size
+        with open(self.corpus_path, 'r', encoding=self.encoding) as fin:
+            for line in fin:
+                # tokenize
+                words = tokenize(line, ngram_size)
+
+                # update vocabulary
+                self.vocabulary |= set(words)
+
+                # skip lines that are too short to produce proper ngrams
+                if len(words) < ngram_size - 1:
+                    continue
+
+                # yield ngrams of size ngram_size and (ngram_size - 1)
+                for offset in range(0, len(words) - ngram_size + 1):
+                    yield tuple(words[offset:offset + ngram_size])
+                    yield tuple(words[offset:offset + ngram_size - 1])
+
+        self.vocab_size = len(self.vocabulary)
 
 
-def save_model(model, model_type, filepath):
+def save_model(model, model_type, vocab_size, filepath):
     """
     Helper function for saving a trained language model to a given location.
 
@@ -76,6 +106,8 @@ def save_model(model, model_type, filepath):
         the model to be saved
     model_type: str
         Simple / CountSketch / CountMinSketch
+    vocab_size: int
+        the vocabulary size
     filepath: str
         the location to save the model
 
@@ -85,6 +117,7 @@ def save_model(model, model_type, filepath):
         pickle.dump({
             'type': model_type,
             'counter': model,
+            'vocab_size': vocab_size,
             'hash_size': args.hash_size,
             'hash_num': args.hash_num,
             'ngram_size': args.ngram_size,
@@ -92,61 +125,91 @@ def save_model(model, model_type, filepath):
 
 
 def main():
-    # load the input corpus
-    reader = corpus_reader(
-        args.infile, ngram_size=args.ngram_size, encoding=args.encoding)
-
     # choose the counting method base on args
     if args.accurate:
         counter = frequency_estimation.Simple()
+        model_type = 'naive'
     elif args.count_sketch:
         counter = frequency_estimation.CountSketch(
             hash_num=args.hash_num, hash_size=args.hash_size)
+        model_type = 'count_sketch'
     else:
         counter = frequency_estimation.CountMinSketch(
             hash_num=args.hash_num, hash_size=args.hash_size)
+        model_type = 'count_min_sketch'
 
-    for ngram in reader:
+    # load the input corpus
+    reader = CorpusReader(
+        args.infile, ngram_size=args.ngram_size, encoding=args.encoding)
+
+    for i, ngram in enumerate(reader):
+        logging.debug('processing %s' % str(ngram))
         counter.process(ngram)
+        if (i + 1) % 100000 == 0:
+            logging.info('processed %d ngrams' % (i + 1))
 
     # save the model for future evaluation
-    save_model(counter, type(counter), args.output)
+    save_model(counter, model_type, reader.vocab_size, args.output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile',
-                        help='the corpus file used to train the model')
+                        help='the corpus file used to train the model'
+                        )
     parser.add_argument('-o', '--output',
                         type=str,
-                        default=os.path.join('.', 'models', 'model'),
-                        help='location to save the trained model (default: ./models/count_min_sketch')
+                        default=os.path.join(
+                            '.', 'models', 'count_min_sketch'),
+                        help='location to save the trained model (default: ./models/count_min_sketch'
+                        )
     parser.add_argument('--encoding',
                         type=str,
                         default='utf-8',
-                        help='the encoding of the corpus file (default: utf-8)')
+                        help='the encoding of the corpus file (default: utf-8)'
+                        )
     parser.add_argument('-hn', '--hash_num',
                         type=int,
                         default=32,
-                        help='the number of hash functions to use (default: 32)')
+                        help='the number of hash functions to use (default: 32)'
+                        )
     parser.add_argument('-hs', '--hash_size',
                         type=int,
                         default=65536,
-                        help='the size of the hash values')
+                        help='the size of the hash values'
+                        )
     parser.add_argument('-ns', '--ngram_size',
                         type=int,
                         default=5,
-                        help='ngrams of size ngram_size - 1 and ngram_size will be counted (default: 5)')
+                        help='ngrams of size ngram_size - 1 and ngram_size will be counted (default: 5)'
+                        )
+    parser.add_argument('-v', '--verbose',
+                        help='increase verbosity',
+                        action='store_const',
+                        dest='loglevel',
+                        const=logging.INFO,
+                        default=logging.WARNING
+                        )
+    parser.add_argument('-d', '--debug',
+                        help='increase verbosity',
+                        action='store_const',
+                        dest='loglevel',
+                        const=logging.DEBUG,
+                        )
 
     # args to determine which counting method to use (deafult:
     # Count_Min_Sketch)
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-a', '--accurate',
                        action='store_true',
-                       help='use naive accurate counting  (probabilistic counting is used by default)')
+                       help='use naive accurate counting  (probabilistic counting is used by default)'
+                       )
     group.add_argument('--count_sketch',
                        action='store_true',
-                       help='use Count_Sketch (the default counter is Count_Min_Sketch)')
+                       help='use Count_Sketch (the default counter is Count_Min_Sketch)'
+                       )
 
     args = parser.parse_args()
+
+    logging.basicConfig(level=args.loglevel)
     main()
